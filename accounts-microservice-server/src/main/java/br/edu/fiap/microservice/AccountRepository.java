b6ce1785265cd3d863342e4a8841bf45eb9aec03
Thiago Souza Cardoso <thiago.cardosok@gmail.com>
2017-03-08 16:44:12 -0300

    commit inicial

---
+++ accounts-microservice-server/src/main/java/br/edu/fiap/microservice/AccountRepository.java
@@ -0,0 +1,17 @@
+package br.edu.fiap.microservice;
+
+import java.util.List;
+
+public class AccountRepository {
+
+	public List<Account> getAllAccounts() {
+
+		return null;
+	}
+
+	public Account getAccount(String id) {
+
+		return null;
+	}
+
+}
